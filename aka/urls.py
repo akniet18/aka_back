ea2b46910243addf6539aa8511bb2915aa3f39ca
Akniet <akniet1805@gmail.com>
2019-11-25 22:01:52 +0600

    static files

--- aka/urls.py
+++ aka/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,4 +25,6 @@ urlpatterns = [
     path('users/', include('users.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
-urlpatterns += staticfiles_urlpatterns()
+if settings.DEBUG: 
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
