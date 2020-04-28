91b5e1ffa8901ada81357fdcd14ebd7110687606
akniet18 <akniet.akniet2016@mail.ru>
2020-04-28 20:56:04 +0600

    asd

--- aka/settings.py
+++ aka/settings.py
@@ -124,24 +124,24 @@ WSGI_APPLICATION = 'aka.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'aka',
-#         'USER': 'postgres',
-#         'PASSWORD': 'pwd123',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'aka',
+        'USER': 'postgres',
+        'PASSWORD': 'pwd123',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 
 
 
